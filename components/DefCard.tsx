fa081c3c7cacdb14ad4a6363df83e8008bd64a3c
unknown <seifmthabet@gmail.com>
2023-11-21 16:45:08 +0200

    first

--- components/DefCard.tsx
+++ components/DefCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DefCard = () => {
     return (
         <div>
-            
+            DefCard
         </div>
     )
 }
--- components/WordsCard.tsx
+++ components/WordsCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WordsCard = () => {
     return (
         <div>
-            
+            Word Card
         </div>
     )
 }
