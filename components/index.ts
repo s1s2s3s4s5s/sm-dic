15a5f0b732671477e9b25f7a63ce1392e27508ec
unknown <seifmthabet@gmail.com>
2023-11-21 16:48:12 +0200

    fisrt

--- components/index.ts
+++ components/index.ts
@@ -3,11 +3,15 @@ import Footer from "./Footer"
 import CustomButton from "./CustomButton"
 import QuoteCard from "./QuoteCard"
 import Searchbar from "./Searchbar"
+import DefCard from "./DefCard"
+import WordsCard from "./WordsCard"
 
 export {
     Navbar,
     Footer,
     CustomButton,
     QuoteCard,
-    Searchbar
+    Searchbar,
+    DefCard,
+    WordsCard
 }
