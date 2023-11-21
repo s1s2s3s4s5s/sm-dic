f67829883a439a71cd1421276b77dfc79f7311c5
unknown <seifmthabet@gmail.com>
2023-11-21 16:08:31 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: "jit",
   theme: {
     extend: {
       fontFamily: {
@@ -15,17 +15,16 @@ const config: Config = {
         "black-100": "#2B2C35",
         "primary-blue": {
           DEFAULT: "#2B59FF",
-          100: "#F5F8FF"
+          100: "#F5F8FF",
         },
         "secondary-orange": "#f79761",
         "light-white": {
-          DEFAULT: "rgba(59, 60, 152, 0.03)",
-          100: "rgba(59, 60, 152, 0.02)"
+          DEFAULT: "rgba(59,60,152,0.03)",
+          100: "rgba(59,60,152,0.02)",
         },
-        "gray": "#747A88",
+        grey: "#747A88",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
